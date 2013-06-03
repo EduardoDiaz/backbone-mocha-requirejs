64c9eda7c489912b1f71bfaad0f680015818ffde
Eduardo Diaz <eduar.diaz37@gmail.com>
2013-06-03 17:11:22 -0400

    test

--- test/browser/test/test.js
+++ test/browser/test/test.js
@@ -8,5 +8,9 @@ define([
             it('should return Hello World', function () {
                 expect(hello()).to.equal('Hello World');
             });
+
+            it('should return 4', function () {
+                expect(2+2).to.equal(4);
+            });
         });
     });
